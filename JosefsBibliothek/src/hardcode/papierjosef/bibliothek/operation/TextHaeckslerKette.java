f307eb12685bedb932d512a7f1f4d480a9190616
Dominik <doglenz@web.de>
2015-05-01 15:39:57 +0200

    fuehreAus hat jz die Residuals mit drin

--- JosefsBibliothek/src/hardcode/papierjosef/bibliothek/operation/TextHaeckslerKette.java
+++ JosefsBibliothek/src/hardcode/papierjosef/bibliothek/operation/TextHaeckslerKette.java
@@ -1,8 +1,52 @@
 package hardcode.papierjosef.bibliothek.operation;
 
+import hardcode.papierjosef.bibliothek.operation.rules.nlp.NlpAbschluss;
+import hardcode.papierjosef.bibliothek.operation.rules.nlp.Paragraphizer;
+import hardcode.papierjosef.bibliothek.operation.rules.nlp.SentenceDetector;
+import hardcode.papierjosef.bibliothek.operation.rules.nlp.TokenDetector;
+import hardcode.papierjosef.model.document.Document;
+import hardcode.papierjosef.model.document.HumbugException;
+import hardcode.papierjosef.model.document.Paragraph;
+import hardcode.papierjosef.model.document.Residual;
+import hardcode.papierjosef.model.document.Sentence;
+
+import java.util.List;
+
 public class TextHaeckslerKette {
-	// TODO
-	// Besonders hier: TextHaecksler hat ein- und ausgabe
-	// Jetzt also: Ausgabe des vorherigen Haeckslers an das nächste Kettenglied
-	// als Eingabe geben.
+
+	public Document execute(String plainText, List<Residual> residuals) {
+
+		try {
+			Document d = new Document(null, 0, 0);
+
+			Paragraphizer p = new Paragraphizer();
+			SentenceDetector s = new SentenceDetector();
+			TokenDetector t = new TokenDetector();
+			NlpAbschluss n = new NlpAbschluss();
+
+			p.setRawText(plainText);
+			p.fuehreAus(d);
+
+			List<String> stringParagraphs = p.getOutput();
+			List<Paragraph> paragraphs = d.getChildElements();
+
+			for (int i = 0; i < stringParagraphs.size(); i++) {
+				s.setRawText(stringParagraphs.get(i));
+				s.fuehreAus(paragraphs.get(i));
+				List<String> stringSentences = s.getOutput();
+				List<Sentence> sentences = paragraphs.get(i).getChildElements();
+				for (int j = 0; j < stringSentences.size(); j++) {
+					t.setRawText(stringSentences.get(j));
+					t.fuehreAus(sentences.get(j));
+				}
+			}
+
+			n.fuehreAus(d);
+
+			return d;
+		} catch (HumbugException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
