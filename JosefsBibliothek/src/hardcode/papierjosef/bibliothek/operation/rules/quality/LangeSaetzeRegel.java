9a372140ab5229d6f95407d512196e6155ed5605
Dominik <doglenz@web.de>
2015-05-06 16:20:29 +0200

    Jz gt das gleiche auch bei AnalyzeTab - so mit Argumenten und so

--- JosefsBibliothek/src/hardcode/papierjosef/bibliothek/operation/rules/quality/LangeSaetzeRegel.java
+++ JosefsBibliothek/src/hardcode/papierjosef/bibliothek/operation/rules/quality/LangeSaetzeRegel.java
@@ -5,17 +5,23 @@ import hardcode.papierjosef.model.document.Sentence;
 import hardcode.papierjosef.model.document.annotation.TextElementProperty;
 
 public class LangeSaetzeRegel extends Regel<Sentence> {
+
+	public LangeSaetzeRegel() {
+		super();
+		arg("Satzlaenge", "20");
+	}
+
 	@Override
 	public void execute(Sentence t) {
-		int maxLength = 20;
+		int maxLength = p("Satzlaenge", Integer.class);
 		if (t.size() > maxLength) {
 			TextElementProperty tep = new TextElementProperty("LONG_SENTENCE",
 					"Langer Satz");
 			t.addProperty(tep);
 		}
 	}
-	
-	public String getProperty(){
+
+	public String getProperty() {
 		return "LONG_SENTENCE";
 	}
 }
